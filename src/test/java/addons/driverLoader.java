b959d54242e083aebcf59e75fb9634e39779ce18
Alan <alan@iuniadmins-MacBook-Pro.local>
2018-11-02 20:48:28 -0300

    force a commit for building travis

--- src/test/java/addons/driverLoader.java
+++ src/test/java/addons/driverLoader.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
+//
 public class driverLoader {
 
     public static WebDriver ffHeadless() {    	
